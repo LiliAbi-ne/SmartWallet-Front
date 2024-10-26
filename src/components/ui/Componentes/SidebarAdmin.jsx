c054c39695a606514f545a081a9b7aaf3ea05d99
David <avilagarciajesusdavid41@gmail.com>
2024-10-26 10:34:45 -0600

    Enlaces al sidebar y componentes de admin creados

--- src/components/ui/Componentes/SidebarAdmin.jsx
+++ src/components/ui/Componentes/SidebarAdmin.jsx
@@ -1,16 +1,9 @@
-import {
-  FileText,
-  Target,
-  GraduationCap,
-  BarChart2,
-  User,
-  CircleDollarSign,
-} from "lucide-react";
+import { FileText, GraduationCap, BarChart2, User, Users } from "lucide-react";
 import PropTypes from "prop-types";
 import Logo from "../../../assets/Logo.png";
 import { Link } from "react-router-dom";
 
-export default function Sidebar() {
+export default function SidebarAdmin() {
   return (
     <aside className="w-64 h-screen bg-[#F5F5F5] shadow-lg flex flex-col">
       <div className="p-4 flex items-center space-x-2">
@@ -22,28 +15,15 @@ export default function Sidebar() {
           <MenuItem
             icon={<FileText size={20} />}
             label="Descripción"
-            to={"/user-overview"}
+            to={"/"}
           />
-          <MenuItem
-            icon={<CircleDollarSign size={20} />}
-            label="Gastos"
-            to={"/expenses"}
-          />
-          <MenuItem
-            icon={<Target size={20} />}
-            label="Metas"
-            to={"/goals"}
-          />
+          <MenuItem icon={<Users size={20} />} label="Usuarios" to={"/"} />
           <MenuItem
             icon={<GraduationCap size={20} />}
             label="Educación"
-            to={"/education"}
+            to={"/"}
           />
-          <MenuItem
-            icon={<BarChart2 size={20} />}
-            label="Reportes"
-            to={"/reports"}
-          />
+          <MenuItem icon={<BarChart2 size={20} />} label="Reportes" to={"/"} />
         </ul>
       </nav>
       <div className="p-4 border-t">
@@ -51,7 +31,7 @@ export default function Sidebar() {
           <div className="w-8 h-8 bg-gray-300 rounded-full flex items-center justify-center">
             <User size={20} />
           </div>
-          <span className="text-sm font-medium">Usuario panel</span>
+          <span className="text-sm font-medium">Admin panel</span>
         </div>
       </div>
     </aside>
@@ -76,5 +56,5 @@ function MenuItem({ icon, label, to }) {
 MenuItem.propTypes = {
   icon: PropTypes.node.isRequired, // Validar que 'icon' sea un nodo de React (puede ser un componente o un elemento JSX)
   label: PropTypes.string.isRequired, // Validar que 'label' sea una cadena de texto
-  to: PropTypes.string.isRequired, 
+  to: PropTypes.string.isRequired,
 };
